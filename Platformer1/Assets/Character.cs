3daec75a0efe55ae2144255ef999638c1564eee3
arschen <plamen_daskalov_1986@abv.bg>
2017-02-24 00:06:07 +0200

    master very initial commit 1.0
    Character + platform

---
+++ Platformer1/Assets/Character.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public enum characterState
+    {
+        inAir=0, idle
+    }
+
+    [SerializeField]
+    float jumpVelocity = 5;
+
+    [SerializeField]
+    float walkVelocity = 5;
+
+    Rigidbody2D characterRigidBody;
+
+    characterState charState;
+
+	// Use this for initialization
+	void Start () {
+        characterRigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void HandleInput()
+    {
+        Vector3 position = gameObject.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.W) && charState != characterState.inAir)
+        {
+            characterRigidBody.velocity = new Vector2(characterRigidBody.velocity.x, jumpVelocity);
+            charState = characterState.inAir;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position = new Vector3(position.x - walkVelocity, position.y, position.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position = new Vector3(position.x + walkVelocity, position.y, position.z);
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        HandleInput();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Ground") && charState == characterState.inAir)
+        {
+            charState = characterState.idle;
+        }
+    }
+}
---
+++ Platformer1/Assets/platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Physics2D.IgnoreCollision(GameObject.Find("Character").GetComponent<CapsuleCollider2D>(), gameObject.GetComponents<BoxCollider2D>()[1], true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Character")) //&& charState == characterState.inAir)
+        {
+            Vector3 max = gameObject.GetComponents<BoxCollider2D>()[0].bounds.max;
+            if (//collider.gameObject.GetComponentsInChildren<Transform>()[0].position.y >= max.y)
+                collider.gameObject.transform.FindChild("contactPoint").transform.position.y >= max.y)
+            {
+                Physics2D.IgnoreCollision(collider, gameObject.GetComponents<BoxCollider2D>()[1], false);
+                //charState = characterState.idle;
+            }
+            else
+            {
+                Physics2D.IgnoreCollision(collider, gameObject.GetComponents<BoxCollider2D>()[1], true);
+            }
+        }
+    }
+}
