24f8ebadf5ebfc2408e0cbf649a7e62d9009430f
arschen <plamen_daskalov_1986@abv.bg>
2017-02-25 20:26:12 +0200

    CameraFollow.cs

---
+++ Platformer1/Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    GameObject objectToFollow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        gameObject.transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y, gameObject.transform.position.z);
+	}
+}
