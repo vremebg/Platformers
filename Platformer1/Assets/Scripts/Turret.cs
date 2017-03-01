eaabc8b1e7a1de9ee12ba1dd45c235d95679649c
arschen <plamen_daskalov_1986@abv.bg>
2017-03-01 09:25:29 +0200

    Added Turret prefab and 2 instances on the stage. Some renaming of files.

---
+++ Platformer1/Assets/Scripts/Turret.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    [SerializeField]
+    float range = 5;
+
+    [SerializeField]
+    int framesPerShot = 10;
+
+    [SerializeField]
+    string targetTags;
+
+    [SerializeField]
+    Transform ammo;
+
+    [SerializeField]
+    Transform shootingPoint;
+
+    [SerializeField]
+    Transform cannon;
+
+    Animator turretAnimator;
+
+    string[] tags;
+
+    int counter = 10;
+
+    void Start()
+    {
+        tags = targetTags.Split(',');
+        turretAnimator = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        counter++;
+        if (counter >= framesPerShot)
+            counter = framesPerShot;
+
+        if (counter >= framesPerShot)
+        {
+            Collider2D[] targetsInArea = Physics2D.OverlapCircleAll(this.transform.position, range);
+            if (targetsInArea.Length != 0)
+            {
+                bool stopCheck = false;
+                foreach (Collider2D collidingObject in targetsInArea)
+                {
+                    foreach (string tag in tags)
+                        if (collidingObject.CompareTag(tag))
+                        {
+                            float dx = collidingObject.gameObject.transform.position.x - shootingPoint.position.x;
+                            float dy = collidingObject.gameObject.transform.position.y - shootingPoint.position.y;
+                            if (cannon.position.x - collidingObject.gameObject.transform.position.x < 0 && this.transform.localScale.x >= 0)
+                                this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y);
+                            if (cannon.position.x - collidingObject.gameObject.transform.position.x > 0 && this.transform.localScale.x <= 0)
+                                this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y);
+                            cannon.rotation = Quaternion.Euler(0, 0, Mathf.Atan((cannon.position.y - collidingObject.gameObject.transform.position.y) / (cannon.position.x - collidingObject.gameObject.transform.position.x)) * 180 / Mathf.PI);
+                            Instantiate(ammo, shootingPoint.position, Quaternion.Euler(0, 0, Mathf.Atan(dy/dx)*180/Mathf.PI));
+                            GameObject[] ammos = GameObject.FindGameObjectsWithTag("Turret_ammo");
+                            ammos[ammos.Length - 1].GetComponent<TurretAmmo>().setInitialVelocity(dx, dy);
+                            if (dx < 0)
+                                ammos[ammos.Length - 1].transform.localScale = new Vector2(ammos[ammos.Length - 1].transform.localScale.x * -1, ammos[ammos.Length - 1].transform.localScale.y);
+                            counter = 0;
+                            stopCheck = true;
+                            break;
+                        }
+                    if (stopCheck)
+                        break;
+                }
+            }
+        }
+        turretAnimator.SetBool("TurretShoot", counter == 0);
+    }
+}
---
+++ Platformer1/Assets/Scripts/TurretAmmo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAmmo : MonoBehaviour {
+
+    [SerializeField]
+    GameObject ammoExplosion;
+
+    [SerializeField]
+    string tagsForExploding;
+
+    [SerializeField]
+    float speed = 3;
+
+    [SerializeField]
+    float lifetimeInSeconds = 2;
+
+    string[] tags;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetimeInSeconds);
+        tags = tagsForExploding.Split(',');
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Character"))
+            gameObject.GetComponent<DamageDealer>().applyDamageOnce(collider.gameObject);
+        if (tags != null && tags.Length != 0)
+            foreach (string tag in tags)
+                if (collider.gameObject.CompareTag(tag))
+                {
+                    Instantiate(ammoExplosion, gameObject.transform.position, Quaternion.Euler(0, 0, 0));
+                    DestroyObject(gameObject);
+                }
+    }
+
+    //dx,dy are distance between shootingPoint and Target
+    public void setInitialVelocity(float dx, float dy)
+    {
+        float yVel = speed * dy / Mathf.Sqrt(Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2));
+        float xVel = speed * dx / Mathf.Sqrt(Mathf.Pow(dx, 2) + Mathf.Pow(dy, 2));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xVel, yVel);
+    }
+}
