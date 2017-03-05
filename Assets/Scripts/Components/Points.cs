50c293c45ae8c60b565bcf0c00ac9167b131e293
arschen <plamen_daskalov_1986@abv.bg>
2017-03-05 15:45:57 +0200

    Added Points component to Character.prefab + on hud representation

---
+++ Assets/Scripts/Components/Points.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour {
+
+    [SerializeField]
+    private int points;
+
+    [SerializeField]
+    private int powerUpPoints;
+
+    [SerializeField]
+    private GameObject hudComponent;
+
+    [SerializeField]
+    private bool isOnHud = false;
+
+    void Start()
+    {
+        if (isOnHud)
+            hudComponent.GetComponent<Text>().text = "Points " + points.ToString();
+    }
+
+    public float getPoints()
+    {
+        return points;
+    }
+
+    public void changePoints(int change)
+    {
+        points += change;
+        if (points < 0)
+            points = 0;
+        if (isOnHud)
+            hudComponent.GetComponent<Text>().text = "Points " + points.ToString();
+    }
+
+    public bool pointsDepleted()
+    {
+        if (points<=0) return true;
+        else return false;
+    }
+
+    public bool isPoweredUp()
+    {
+        if (points >= powerUpPoints) return true;
+        else return false;
+    }
+}
