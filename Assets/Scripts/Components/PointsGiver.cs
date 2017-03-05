02896c11e3fc3597deb8ef002968590cdcef27e4
arschen <plamen_daskalov_1986@abv.bg>
2017-03-05 16:45:54 +0200

    Added coins + coin pick

---
+++ Assets/Scripts/Components/PointsGiver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsGiver : MonoBehaviour {
+
+    [SerializeField]
+    int points;
+
+    public void givePointsOnce(GameObject receiver)
+    {
+        receiver.GetComponent<Points>().changePoints(points);
+    }
+
+    public void givePointsOnce(string receiver)
+    {
+        GameObject.Find(receiver).GetComponent<Points>().changePoints(points);
+    }
+
+    public bool isPointsPoweredUp(GameObject receiver)
+    {
+        return receiver.GetComponent<Points>().isPoweredUp();
+    }
+
+    public bool isPointsPoweredUp(string receiver)
+    {
+        return GameObject.Find(receiver).GetComponent<Points>().isPoweredUp();
+    }
+}
