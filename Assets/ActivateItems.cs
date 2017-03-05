4750e1f3823502c289bd782c5fd02330ab3d532b
arschen <plamen_daskalov_1986@abv.bg>
2017-03-05 22:01:26 +0200

    Objects activation/disabling based on distance to character ( check Scene while game is running ). Ignores Root/Character/Untagged/MainCamera. Requires Root element above all others and canvas to determine distances. Seems to work but is highly toxic and can potentially break everything. Added a black_hole.png for teleporter ( To Do ).

---
+++ Assets/ActivateItems.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateItems : MonoBehaviour {
+
+    [SerializeField]
+    GameObject rootObject;
+
+    [SerializeField]
+    Canvas checkSize;
+
+    float x;
+    float y;
+
+    void Start()
+    {
+        x = checkSize.GetComponent<RectTransform>().rect.width * checkSize.GetComponent<RectTransform>().localScale.x;
+        y = checkSize.GetComponent<RectTransform>().rect.height * checkSize.GetComponent<RectTransform>().localScale.y;
+    }
+
+    void Update()
+    {
+        foreach(Transform temp in rootObject.GetComponentsInChildren<Transform>(true))
+        {
+            if (!temp.gameObject.CompareTag("Character") && !temp.gameObject.CompareTag("Root")
+                && !temp.gameObject.CompareTag("Untagged") && !temp.gameObject.CompareTag("MainCamera"))
+                if (temp.position.x + x> gameObject.transform.position.x && temp.position.x - x < gameObject.transform.position.x)
+                {
+                    if (temp.position.y + y > gameObject.transform.position.y && temp.position.y - y < gameObject.transform.position.y)
+                        temp.gameObject.SetActive(true);
+                    else
+                        temp.gameObject.SetActive(false);
+                }
+                else
+                    temp.gameObject.SetActive(false);
+        }
+    }
+}
