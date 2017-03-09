286debea33bb911599593792ffbd0dc5b290c1c0
arschen <plamen_daskalov_1986@abv.bg>
2017-03-09 09:51:31 +0200

    Started work on restarting level and in-game menu

---
+++ Assets/Scripts/LosingCondition.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LosingCondition : MonoBehaviour {
+
+    [SerializeField]
+    GameObject character;
+
+    [SerializeField]
+    bool pointsObjective;
+
+    [SerializeField]
+    int pointsCount;
+
+    [SerializeField]
+    bool healthObjective;
+
+    [SerializeField]
+    int healthTreshold;
+
+    [SerializeField]
+    GameObject lossMenu;
+
+    Health healthComponent;
+    Points pointsComponent;
+
+    // Use this for initialization
+    void Start()
+    {
+        healthComponent = character.GetComponent<Health>();
+        pointsComponent = character.GetComponent<Points>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthObjective)
+            if (healthComponent.getHealth() <= healthTreshold)
+            {
+                lossMenu.SetActive(true);
+            }
+        if (pointsObjective)
+            if (pointsComponent.getPoints() <= pointsCount)
+            {
+                lossMenu.SetActive(true);
+            }
+    }
+}
