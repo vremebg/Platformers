b49774b822b19c627e5eba8a67c6977520690dbb
arschen <plamen_daskalov_1986@abv.bg>
2017-03-11 07:58:51 +0200

    Added in game menu with Resume/Restart/Quit

---
+++ Assets/Scripts/IGM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IGM : MonoBehaviour {
+
+    [SerializeField]
+    GameObject IGMenu;
+
+    public void IGMPopUp()
+    {
+        IGMenu.transform.parent.gameObject.SetActive(true);
+        IGMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void IGMResume()
+    {
+        Time.timeScale = 1;
+        gameObject.transform.parent.gameObject.SetActive(false);
+        gameObject.transform.parent.gameObject.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void IGMRestart()
+    {
+        foreach (GameObject temp in SceneManager.GetActiveScene().GetRootGameObjects())
+            temp.SetActive(false);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void IGMQuit()
+    {
+        Application.Quit();
+    }
+}
