248f0ef751077d481f8d3cac226f2aaf1f44ff3d
arschen <plamen_daskalov_1986@abv.bg>
2017-03-21 10:07:56 +0200

    Added invincibility after hit + blinking, configurable in Health component

--- Assets/Scripts/Components/Health.cs
+++ Assets/Scripts/Components/Health.cs
@@ -26,10 +26,51 @@ public class Health : MonoBehaviour {
     [SerializeField]
     private GameObject healthSpriteObj;
 
+    [SerializeField]
+    private bool hasInvincibilityAfterHit = false;
+
+    [SerializeField]
+    private float invincibilityInSeconds;
+
+    private bool isInvincible = false;
+    private float invicStartTime;
+    private SpriteRenderer objSpriteRenderer;
+    private float alpha;
+    private float alphaStep = -5;
+
     void Start()
     {
         if (isOnHud)
             hudComponent.GetComponent<Text>().text = ((int)health).ToString();
+        objSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (isInvincible)
+        {
+            if (Time.time - invincibilityInSeconds > invicStartTime)
+            {
+                isInvincible = false;
+                alpha = 255;
+                objSpriteRenderer.color = new Color(255, 255, 255, alpha);
+            }
+            else
+            {
+                alpha += alphaStep;
+                if (alpha < 0)
+                {
+                    alpha = 0;
+                    alphaStep = 0.05f;
+                }else
+                    if (alpha > 1)
+                {
+                    alpha = 1;
+                    alphaStep = -0.05f;
+                }
+                objSpriteRenderer.color = new Color(255, 255, 255, alpha);
+            }
+        }
     }
 
     public float getHealth()
@@ -39,18 +80,41 @@ public class Health : MonoBehaviour {
 
     public void changeHealth(float change)
     {
-        health += change;
-        if (health > maxHealth)
-            health = maxHealth;
-        if (health < 0)
-            health = 0;
-        if (isOnHud)
-            hudComponent.GetComponent<Text>().text = ((int)health).ToString();
+        if (!hasInvincibilityAfterHit)
+        {
+            health += change;
+            if (health > maxHealth)
+                health = maxHealth;
+            if (health < 0)
+                health = 0;
+            if (isOnHud)
+                hudComponent.GetComponent<Text>().text = ((int)health).ToString();
 
-        if(showDamageNumbersOnChange)
-            gameObject.GetComponent<DamageNumbers>().addNumberToDisplay((int)change);
-        if (showHealth)
-            healthSpriteObj.transform.localScale = new Vector2(health/maxHealth, healthSpriteObj.transform.localScale.y);
+            if (showDamageNumbersOnChange)
+                gameObject.GetComponent<DamageNumbers>().addNumberToDisplay((int)change);
+            if (showHealth)
+                healthSpriteObj.transform.localScale = new Vector2(health / maxHealth, healthSpriteObj.transform.localScale.y);
+        }
+        else
+        {
+            if (!isInvincible)
+            {
+                isInvincible = true;
+                invicStartTime = Time.time;
+                health += change;
+                if (health > maxHealth)
+                    health = maxHealth;
+                if (health < 0)
+                    health = 0;
+                if (isOnHud)
+                    hudComponent.GetComponent<Text>().text = ((int)health).ToString();
+
+                if (showDamageNumbersOnChange)
+                    gameObject.GetComponent<DamageNumbers>().addNumberToDisplay((int)change);
+                if (showHealth)
+                    healthSpriteObj.transform.localScale = new Vector2(health / maxHealth, healthSpriteObj.transform.localScale.y);
+            }
+        }
     }
 
     public bool healthDepleted()
